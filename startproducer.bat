55bb5db77373767debaf11807d2c6c9bc2fd0a76
rtarar <rtarar@gmail.com>
2019-07-31 14:48:22 -0400

    Update startproducer.bat

--- startproducer.bat
+++ startproducer.bat
@@ -5,8 +5,8 @@ set ERROR_TOPIC_NAME=eip-error
 set KAFKA_BROKERS=sdl-dev01.biotech.cdc.gov:9092
 set POLL_INTERVAL_MILLIS=2000
 set S3_BUCKET_NAME=eip-plus-messages-dev
-set S3_ACCESS_KEY=AKIAIHX6U5QGJDZGPVIA
-set S3_SECRET=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
+set S3_ACCESS_KEY=<>
+set S3_SECRET=<>
 set SQS_URL=https://sqs.us-east-1.amazonaws.com/626636711996/eip-legacy-dev
 set SCHEMA_REGISTRY_URL=http://sdl-dev01.biotech.cdc.gov:8081
 
